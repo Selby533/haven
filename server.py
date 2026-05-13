77d549913a31e743af5dc6e0e77556c853dba179
Selby533 <visualized.v@gmail.com>
2026-05-13 16:19:08 +0200

    Add sexual orientation, positive since, height, ethnicity, religion

--- server.py
+++ server.py
@@ -1012,6 +1012,8 @@ def get_cities(country:str):
 
 app.include_router(api_router)
 
+
+
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT",8000)))
